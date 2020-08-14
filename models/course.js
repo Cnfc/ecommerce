6c56f940188851f4c679fa81ddde261c5d04446f
Stanislav Dashkov <stdashkov@mail.ru>
2020-08-14 20:59:03 +0300

    course route

---
+++ models/course.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const courseSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: true,
+    maxlength: 200,
+  },
+  category: {
+    type: String,
+    required: true,
+    enum: ["web", "mobile", "network"],
+    lowercase: true,
+  },
+  isPublished: Boolean,
+  price: {
+    type: Number,
+    required: function () {
+      return this.isPublished;
+    },
+    min: 10,
+    max: 200,
+    get: (v) => Math.round(v),
+    set: (v) => Math.round(v),
+  },
+  author: String,
+  tags: [String],
+  date: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model("Course", courseSchema);
--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const config = require("config");
 const debug = require("debug")("app:startup");
 require("dotenv").config();
 
+const Courses = require("./models/course");
 // import Routes
 const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/user");
@@ -35,18 +36,18 @@ mongoose
 
 // MiddleWare ==================================
 
-if (app.get("env") === "development") {
-  app.use(morgan("dev"));
-  debug("Morgan enabled...");
-}
+// if (app.get("env") === "development") {
+//   app.use(morgan("dev"));
+//   debug("Morgan enabled...");
+// }
 
-console.log("");
-console.log("============== Config =========================");
-console.log("Application Name:" + config.get("name"));
-console.log("Mail Server Name:" + config.get("mail.host"));
-console.log("Mail Password:" + config.get("mail.password"));
-console.log("============== Config =========================");
-console.log("");
+// console.log("");
+// console.log("============== Config =========================");
+// console.log("Application Name:" + config.get("name"));
+// console.log("Mail Server Name:" + config.get("mail.host"));
+// console.log("Mail Password:" + config.get("mail.password"));
+// console.log("============== Config =========================");
+// console.log("");
 
 app.use(bodyParser.json());
 app.use(cookieParser());
@@ -62,6 +63,45 @@ app.use("/api", productRoutes);
 app.use("/api", logRouter);
 
 app.use("/api/courses", courses);
+
+async function createCourse() {
+  const course = new Courses({
+    name: "Angular  Sourse Course",
+    category: "Web",
+    author: "Mosh",
+    tags: ["frontend"],
+    isPublished: true,
+    price: 15.8,
+  });
+
+  try {
+    const res = await course.save();
+    console.log(res);
+  } catch (ex) {
+    for (field in ex.errors) console.log(ex.errors[filed].message);
+  }
+}
+
+// createCourse();
+
+async function getCourse() {
+  const courses = await Courses.find().select({ author: 1 });
+  console.log(courses);
+}
+
+// getCourse();
+
+// async function updateCourse(id) {
+//   const course = await Courses.findByIdAndUpdate(id, {
+//     $set: {
+//       author: "Stanislav",
+//       name: "Learn Reactjs",
+//     },
+//   });
+//   console.log(course);
+// }
+
+// updateCourse("5f354469e633d15ae2a034ac");
 
 // =============================================
 const port = process.env.PORT || 8000;
