4386020e4f86890d04a2add97855871f8bfd902b
Stanislav Dashkov <stdashkov@mail.ru>
2020-09-21 17:18:48 +0300

    del comments

--- index.js
+++ index.js
@@ -55,68 +55,6 @@ app.use("/api", productRoutes);
 app.use("/api", movies);
 app.use("/api", tasks);
 
-// NODE JS COURSE
-
-// async function createAuthor(name, bio, web) {
-//   const author = new Author({
-//     name,
-//     bio,
-//     web,
-//   });
-
-//   const res = await author.save();
-//   console.log(res);
-// }
-
-// async function createCourse(name, author) {
-//   const course = new Course({
-//     name,
-//     author,
-//   });
-
-//   const res = await course.save();
-//   console.log(res);
-// }
-
-// async function listCourses() {
-//   const courses = await Course.find()
-//     .populate("author", "name -_id")
-//     .select("author name");
-//   console.log(courses);
-// }
-
-async function createGenres(name) {
-  const genre = new Genres({
-    name,
-  });
-
-  const res = await genre.save();
-  console.log(res);
-}
-
-async function createMovies(title, genre, numberInStock, dailyRentalRate) {
-  const movie = new Movies({
-    title,
-    genre,
-    numberInStock,
-    dailyRentalRate,
-  });
-
-  const res = await movie.save();
-  console.log(res);
-}
-
-async function listMovies() {
-  const movies = await Movies.find();
-  console.log(movies);
-}
-
-// Routes
-
-// createGenres("Fantasy");
-// createMovies("SAO", "5f3a886f807e4d41a459ef39", 0, 0);
-// listMovies();
-// getProducts();
 // =============================================
 const port = process.env.PORT || 7999;
 app.listen(port, () => {
