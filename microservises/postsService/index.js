a019aa5a26891e1726c2fdf3e11fa6494c91279a
Stanislav Dashkov <stdashkov@mail.ru>
2021-02-18 16:15:13 +0300

    add eventBus Pod in NodePort

--- microservises/postsService/index.js
+++ microservises/postsService/index.js
@@ -20,7 +20,7 @@ app.get("/posts", (req, res) => {
   res.send(posts);
 });
 
-app.post("/posts", async (req, res) => {
+app.post("/posts/create", async (req, res) => {
   const { title } = req.body;
   const id = randomBytes(8).toString("hex");
 
@@ -30,7 +30,7 @@ app.post("/posts", async (req, res) => {
   };
   console.log(posts);
 
-  await axios.post("http://localhost:4005/events", {
+  await axios.post("http://event-bus-srv:4005/events", {
     type: "PostCreated",
     data: { id, title },
   });
